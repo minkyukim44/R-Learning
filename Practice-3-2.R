ebac4c6e323748576a9536ec1b15e698a5de5f20
minkyukim44 <mkimintruth@gmail.com>
2020-07-13 17:32:44 +0900

    Practice Functions

---
+++ Practice-3-2.R
@@ -0,0 +1,20 @@
+# 변수 만들기
+x <- c(1,2,3)
+x
+
+# 함수 적용하기
+mean(x)
+
+max(x)
+
+min(x)
+
+str5 <- c("Hello!","World","is","good!")
+paste(str5, collapse=",")   # 쉼표를 구분자로 str5의 단어들 하나로 합치기
+paste(str5, collapse=" ")
+
+x_mean <- mean(x)
+x_mean
+
+str5_paste <- paste(str5, collapse = " ")
+str5_paste
