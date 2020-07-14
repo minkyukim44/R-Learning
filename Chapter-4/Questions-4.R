26efd86864ed3043f6712550616adf68db03ae78
minkyukim44 <mkimintruth@gmail.com>
2020-07-14 10:54:55 +0900

    Chapter 4 Questions - First

---
+++ Chapter-4/Questions-4.R
@@ -0,0 +1,9 @@
+## Questions
+# Q-4-2
+
+df_fruit <- data.frame(product = c("apple", "strawberry", "watermelon"),
+                       price = c(1800, 1500, 3000),
+                       sales_rate = c(24, 38, 13))
+
+mean(df_fruit$price)        # mean of fruit prices
+mean(df_fruit$sales_rate)   # mean of fruit sales rate
