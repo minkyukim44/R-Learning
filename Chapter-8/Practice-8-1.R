d73bf07c0a08507801bf40af5c1b1de9679bad94
minkyukim44 <mkimintruth@gmail.com>
2020-07-17 17:06:28 +0900

    Practice Chapter 8

--- Chapter-8/Practice-8-1.R
+++ Chapter-8/Practice-8-1.R
@@ -21,5 +21,22 @@ mpg <- as.data.frame(ggplot2::mpg)
 df_mpg <- mpg %>% 
     group_by(drv) %>% 
     summarise(mean_hwy = mean(hwy))
+df_mpg$mean_hwy <- df_mpg$mean_hwy %>% round(df_mpg$mean_hwy,2)
 df_mpg
 
+ggplot(data = df_mpg, aes(x = reorder(drv, -mean_hwy), y = mean_hwy)) +         # reorder : re-arrangement the order
+    geom_col()          # bar plot
+
+ggplot(data = mpg, aes(x = drv)) + geom_bar()       # frequency bar plot
+ggplot(data = mpg, aes(x = hwy)) + geom_bar()
+
+## Line Chart
+       
+library(ggplot2)
+ggplot(data = economics, aes(x = date, y = unemploy)) + geom_line()
+
+## Box Plot
+
+ggplot(data = mpg, aes(x = drv, y = hwy)) + geom_boxplot()
+            
+       
--- Chapter-8/Question-8.R
+++ Chapter-8/Question-8.R
@@ -13,4 +13,28 @@ ggplot(midwest, aes(poptotal, popasian)) +
     xlim(0,5e+5) +
     ylim(0,1e+4)
 
+# Q-8-2
 
+mpg
+mpg_cty<- mpg %>% 
+    filter(class == "suv") %>% 
+    group_by(manufacturer) %>% 
+    summarise(mean_cty = mean(cty)) %>% 
+    arrange(desc(mean_cty)) %>% 
+    head(5)
+
+ggplot(data = mpg_cty, aes(x = reorder(manufacturer, -mean_cty), y = mean_cty)) + geom_col()
+ggplot(data = mpg, aes(x = class)) + geom_bar()
+
+# Q-8-3
+
+library(ggplot2)
+
+ggplot(data = economics, aes(x = date, y = psavert)) + geom_line()
+
+# Q-8-4
+
+library(ggplot2)
+
+ggplot(data = mpg, aes(x = class, y = cty)) + geom_boxplot()
+       
