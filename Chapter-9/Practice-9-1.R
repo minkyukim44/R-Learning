7a19f1342df1e23494fa0f5c07fe9f7b1c75edb5
minkyukim44 <mkimintruth@gmail.com>
2020-07-21 14:00:42 +0900

    Practice Chapter - 9 / Update 'R', Install 'foregin'

---
+++ Chapter-9/Practice-9-1.R
@@ -0,0 +1,40 @@
+## Update R version
+install.packages("installr")
+library("installr")
+check.for.updates.R()
+install.R()
+version
+
+
+## Install packages
+install.packages("foreign")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("readxl")
+
+library(foreign)
+library(dplyr)
+library(ggplot2)
+library(readxl)
+
+## Read a file
+raw_welfare <- read.spss(file = "Koweps_hpc10_2015_beta1.sav", to.data.frame = T)
+
+welfare <- raw_welfare
+
+head(welfare)
+tail(welfare)
+View(welfare)
+dim(welfare)
+str(welfare)
+summary(welfare)
+
+# Change column names
+welfare <- rename(welfare, 
+                  sex = h10_g3,
+                  birth = h10_g4,
+                  marriage = h10_g10,
+                  religion = h10_g11,
+                  income = p1002_8aq1,
+                  code_job = h10_eco9,
+                  code_region = h10_reg7)
