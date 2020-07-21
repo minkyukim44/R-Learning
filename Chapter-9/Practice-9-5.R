5ceddda40a84d7956fd7ac67107b4b29b8751955
minkyukim44 <mkimintruth@gmail.com>
2020-07-21 16:56:55 +0900

    Practice Chapte 9 / double variables

---
+++ Chapter-9/Practice-9-5.R
@@ -0,0 +1,24 @@
+## Age group, sex, income
+# Make a derived varaible
+sex_income <- welfare %>% 
+    filter(!is.na(income)) %>% 
+    group_by(ageg, sex) %>% 
+    summarise(mean_income = mean(income))
+sex_income
+
+# Plotting
+ggplot(data = sex_income, aes(x = ageg, y = mean_income, fill = sex)) +         # show both male and female in one column
+    geom_col() +
+    scale_x_discrete(limits = c("young", "middle", "old"))
+
+ggplot(data = sex_income, aes(x = ageg, y = mean_income, fill = sex)) +         # show both male and female in two columns
+    geom_col(position = "dodge") +
+    scale_x_discrete(limits = c("young", "middle", "old"))
+
+## Age, sex, income
+sex_age <- welfare %>% 
+    filter(!is.na(income)) %>% 
+    group_by(age, sex) %>% 
+    summarise(mean_income = mean(income))
+head(sex_age)
+ggplot(data = sex_age, aes(x = age, y = mean_income, col = sex)) + geom_line()
