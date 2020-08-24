c0a063503e127c9806baa111ed592907b800cdca
minkyukim44 <mkimintruth@gmail.com>
2020-08-24 17:58:08 +0900

    practice chapter 4 / apply functions

---
+++ Data Processing and Analysis/Chapter 4/data_manipulation-2.R
@@ -0,0 +1,34 @@
+## Data Manipulation-2
+# 'apply' functions
+# apply()
+sum(1:10)
+d <- matrix(1:9, ncol = 3)
+apply(d, 1, sum)            # MARGIN = 1 means summing by row
+apply(d, 2, sum)            # MARGIN = 2 means summing by col
+
+head(iris)
+apply(iris[, 1:4], 2, sum)
+
+colSums(iris[, 1:4])        # rowSums, rowMeans, colSums, colMeans
+
+# lapply() for list
+(result <- lapply(1:3, function(x)(x*2)))
+result[[1]]
+unlist(result)  # convert a list to a vector
+
+(x <- list(a = 1:3, b = 4:6))
+lapply(x, mean)
+
+str(lapply(iris[, 1:4], mean))      # the result is a list
+str(colMeans(iris[, 1:4]))          # the reulst is returning values
+
+d <- as.data.frame(matrix(unlist(lapply(iris[,1:4], mean)), ncol = 4, byrow = T))
+names(d) <- names(iris[, 1:4])
+str(d)  # the result is a data frame
+
+data.frame(do.call(cbind, lapply(iris[, 1:4], mean))) # shortcut
+
+(x <- list(data.frame(name = "foo", value = 1), data.frame(name = "bar", value = 2)))
+unlist(x)
+do.call(rbind, x)
+          
